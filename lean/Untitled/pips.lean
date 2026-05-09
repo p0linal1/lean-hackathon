dba4d99962b53cfeb26c183b7e52602f22ef025e
Vivek Sarkar <vivek.sarkar@gmail.com>
2026-05-09 11:31:43 -0700

    more defs n shit

--- lean/Untitled/pips.lean
+++ lean/Untitled/pips.lean
@@ -3,17 +3,23 @@ import Std.Data.HashSet
 
 structure Node where
   id : Nat
-deriving Repr, DecidableEq, Hashable
+deriving Repr, DecidableEq, Hashable, BEq
 
 structure Domino where
   left : Nat
   right : Nat
-deriving Repr, DecidableEq
+deriving Repr, DecidableEq, BEq
 
 structure Pip where
   board: Std.HashMap Node (Std.HashSet Node)
-deriving Repr
+deriving Repr, BEq
 
+def adjacent (pip: Pip) (n₁ n₂ : Node) : Bool :=
+  match pip.board.get? n₁ with
+  | some neighbors => neighbors.contains n₂
+  | none => false
+
+def Assignment := Std.HashMap Node (Node × Node)
 
 def Pip.empty : Pip :=
   { board := Std.HashMap.emptyWithCapacity 64 }
@@ -22,9 +28,18 @@ inductive SumConstraintType where
   | eq
   | lt
   | gt
-deriving Repr
+deriving Repr, DecidableEq, BEq, Hashable
 
 inductive Constraint where
-  | sum   (ns : Std.HashSet Node) (c : Nat) (t : SumConstraintType)
-  | equiv (ns : Std.HashSet Node) (c : Nat)
-deriving Repr
+  | sum   (ns : List Node) (c : Nat) (t : SumConstraintType)
+  | equiv (ns : List Node) (c : Nat)
+deriving Repr, BEq, Hashable
+
+def Constraints := Std.HashSet Constraint
+
+-- def assignment_is_valid (pip : Pip) (assignment : Assignment) : Bool :=
+--   assignment.all (fun (node, (left, right)) =>
+--     match pip.board.find? node with
+--     | some neighbors => neighbors.contains left && neighbors.contains right
+--     | none => false
+--   )
